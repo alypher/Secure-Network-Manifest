bbd4949d2020a539d6f093db1df52c379addb8c3
Alypher Mansano <alypher@gmail.com>
2023-06-19 20:23:25 -0300

    first structure

---
+++ README.md
@@ -0,0 +1,9 @@
+## Local Development
+
+First, run `npm i` to install the dependencies.
+
+Then, run `npm dev` to start the development server and visit localhost:3000.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Secure Network Manifest</span>,
+  project: {
+    link: 'https://alypher.com',
+  },
+  docsRepositoryBase: 'https://alypher.com',
+  footer: {
+    text: 'Secure Network Manifest @2023',
+  },
+}
+
+export default config
